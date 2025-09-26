b6864c27c1107837e5cc1e6540889f8614e8a414
CosmicZard <roshan.25b01010320@abes.ac.in>
2025-09-26 15:12:43 +0530

    update

---
+++ print.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+        int main() {
+            cout << "Hello, World!" << endl;
+
+            // cout << " Square using Multiplication: 1 x 1 = " << 1 * 1 << endl;
+            // cout << " Square using Multiplication: 2 x 2 = " << 2 * 2 << endl;
+            // cout << " Square using Multiplication: 3 x 3 = " << 3 * 3 << endl;
+            // }
+
+            cout << " Square using Addition: 8 + 4 = " << 8 + 4 << endl;
+            cout << " Square using Subtraction: 8 - 4 = " << 8 - 4 << endl;
+            cout << " Square using Multiplication: 8 * 4 = " << 4 * 8 << endl;
+            cout << " Square using Division: 8 / 4 = " << 8 / 4 << endl;
+
+            return 0;
+        };
