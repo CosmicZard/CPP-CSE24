693a87f2595276950b4541eeb8cf01d3f20ae566
CosmicZard <roshan.25b01010320@abes.ac.in>
2025-09-30 09:35:47 +0530

    square_root

---
+++ sqroot_pow.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+        int main() {
+            double x = 16.0;
+            cout << "Square Root: " << x << " is " << sqrt(x) << endl;
+            cout << "Power : " << pow(2, 3) << endl;                      // 2^3 = 8
+            return 0;
+        }
