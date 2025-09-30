276607ad47020e0c94387677297e3ed47afc9a8e
CosmicZard <roshan.25b01010320@abes.ac.in>
2025-09-30 09:15:38 +0530

    update

--- defclass.cpp
+++ defclass.cpp
@@ -3,31 +3,62 @@
 using namespace std;
 
         int main() {
-            class car {
+
+            class animal
+            {
                 public:
-                    string brand;
-                    string model;
-                    int year;
+                    string name;
+                    string say;
+                    string color;
+                    
                     void displayInfo() {
-                        cout << brand << " " << model << " " << year << endl;
+                        cout << name << " " << say << " " << color << endl;
                     }
             };
 
-            car car1;
-            car1.brand = "Toyota";  
-            car1.model = "Corolla";
-            car1.year = 2020;
-            cout << car1.brand << " " << car1.model << " " << car1.year << endl;
-            
+            animal animal1;
+            animal1.name = "Dog";
+            animal1.say = "Woof";
+            animal1.color = "Brown";
+            animal1.displayInfo();
+           
+            animal animal2;
+            animal2.name = "Cat";
+            animal2.say = "Meow";
+            animal2.color = "White";
+            animal2.displayInfo();
 
-            car car2;
-            car2.brand = "Honda";  
-            car2.model = "Civic";
-            car2.year = 2019;
-            cout << car2.brand << " " << car2.model << " " << car2.year << endl;
             return 0;
 
-            car1.displayInfo();
-            car2.displayInfo();
+            
+
+
+
+            // class car {
+            //     public:
+            //         string brand;
+            //         string model;
+            //         int year;
+            //         void displayInfo() {
+            //             cout << brand << " " << model << " " << year << endl;
+            //         }
+            // };
+
+            // car car1;
+            // car1.brand = "Toyota";  
+            // car1.model = "Corolla";
+            // car1.year = 2020;
+            // cout << car1.brand << " " << car1.model << " " << car1.year << endl;
+            
+
+            // car car2;
+            // car2.brand = "Honda";  
+            // car2.model = "Civic";
+            // car2.year = 2019;
+            // cout << car2.brand << " " << car2.model << " " << car2.year << endl;
+            // return 0;
+
+            // car1.displayInfo();
+            // car2.displayInfo();
         }
 
