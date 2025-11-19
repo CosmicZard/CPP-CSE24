7b77ecf27873f249afd3ddbba7aa61684a2be112
CosmicZard <roshan.25b01010320@abes.ac.in>
2025-11-20 01:57:35 +0530

    vector & array

--- Part 2/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-            int arr[5] = {10, 20, 30, 40, 50};
-            cout << "Array elements are: " << endl;
-            for (int i = 0; i < 5; i++) {
-                cout << arr[i] << endl;
-            }
