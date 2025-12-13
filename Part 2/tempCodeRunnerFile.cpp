7ca075256db4089c279121adcf8b1deaa8fc4af1
CosmicZard <roshan.25b01010320@abes.ac.in>
2025-12-13 12:22:16 +0530

    .

---
+++ Part 2/tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+    int a, b, choice;
+    cin >> a >> b >> choice;
+
+    switch (choice) {
+        case 1:
+            cout << a + b;
+            break;
+        case 2:
+            cout << a - b;
+            break;
+        case 3:
+            cout << a * b;
+            break;
+        case 4:
+            if (b != 0)
+                cout << a / b;   // integer division
+            else
+                cout << "Division by zero error";
+            break;
+        default:
+            cout << "Invalid choice";
+    }
